de3b18c528338a02e05cab7eecbd6a6882cf4510
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-05-28 19:11:32 +0300

    Add test for intro description

--- test/start.feature
+++ test/start.feature
@@ -5,11 +5,7 @@ Feature: First start of application
 
   Scenario: As a User I want to see buttons with different options
     Then I see {'How many pushups\ncan you do?'} text
+    And I see {'100 Pushups is the program to train your body to go from 1 pushup to 100 consecutive reps in less than two months.\nAfter starting the program and selecting the current training day (it is highlighted), you must put your device on the floor near your nose and when you do one push-up, touch the screen with your nose. The number on the screen will decrease. When the whole approach is completed, you will have a certain time to rest.\nThe workouts are tailored to your starting level. You can change the training program by simply switching to the appropriate tab.\nLet's find out your starting level!'} text
     And I see {'0-5'} text
     And I see {'6-14'} text
     And I see {'15-29'} text
-
-Scenario: User has made a choice
-    Then I tap {'0-5'} text
-    And I wait
-    And I see {'Week 1, Day 1'} text
--- test/start_test.dart
+++ test/start_test.dart
@@ -6,8 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 import './step/the_app_is_running.dart';
 import './step/i_see_text.dart';
-import './step/i_tap_text.dart';
-import './step/i_wait.dart';
 
 void main() {
   Future<void> bddSetUp(WidgetTester tester) async {
@@ -17,15 +15,10 @@ void main() {
     testWidgets('''As a User I want to see buttons with different options''', (tester) async {
       await bddSetUp(tester);
       await iSeeText(tester, 'How many pushups\ncan you do?');
+      await iSeeText(tester, '100 Pushups is the program to train your body to go from 1 pushup to 100 consecutive reps in less than two months.\nAfter starting the program and selecting the current training day (it is highlighted), you must put your device on the floor near your nose and when you do one push-up, touch the screen with your nose. The number on the screen will decrease. When the whole approach is completed, you will have a certain time to rest.\nThe workouts are tailored to your starting level. You can change the training program by simply switching to the appropriate tab.\nLet's find out your starting level!');
       await iSeeText(tester, '0-5');
       await iSeeText(tester, '6-14');
       await iSeeText(tester, '15-29');
-    });
-    testWidgets('''User has made a choice''', (tester) async {
-      await bddSetUp(tester);
-      await iTapText(tester, '0-5');
-      await iWait(tester);
-      await iSeeText(tester, 'Week 1, Day 1');
     });
   });
 }
